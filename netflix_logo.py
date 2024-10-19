3648e1bf042f581cfa27a0654cf8a49981056bd8
pratimaasapkota <pratimasapkota509@gmail.com>
2024-10-19 13:03:55 +0545

    netflix

--- netflix_logo.py
+++ netflix_logo.py
@@ -75,15 +75,6 @@ t.end_fill()
 t.color("red")
 t.fillcolor("red")
 t.begin_fill()
-t.setheading(113)
-t.forward(195)
-t.setheading(0)
-t.forward(31)
-t.setheading(293)
-t.forward(196)
-t.end_fill()
-t.hideturtle()
-
 
 
 
